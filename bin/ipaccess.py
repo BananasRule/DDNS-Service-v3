61b81ba8ca9e8b892d66185d52450c78d058a782
Jacob <54694270+BananasRule@users.noreply.github.com>
2023-11-15 18:00:35 +0800

    Created ip access class and test cases

---
+++ bin/ipaccess.py
@@ -0,0 +1,73 @@
+import logging
+import requests
+from bin import commonexception
+
+
+## GetIP class enables access to a server to determine the current public IP address
+class GetIP:
+
+
+    ## Create the GetIP object
+    # @param primary_server_url The primary server used to get the current public IP. Must return text.
+    # @param fallback_server_url The fallback server used to get the current public IP. Must return text.
+    def __init__(self, primary_server_url, fallback_server_url):
+        self.logger = logging.getLogger(__name__)
+        self._primary_server_url = primary_server_url
+        self._fallback_server_url = fallback_server_url
+        self.logger.debug("Created GetIP class")
+
+    ## Get the server's current public IP address
+    def get(self):
+        success = False
+        response = None
+        # Attempt to get IP address from primary server
+        try:
+            response = requests.get(self._primary_server_url)
+            if response.status_code != 200:
+                raise _GetIPResponseError
+            else:
+                success = True
+
+        # Except an incorrect response
+        except _GetIPResponseError:
+            self.logger.warning("Error with primary IP address server. A response code other than 200 was received. \
+                                Attempting to access fallback server in 30 seconds")
+
+        # Attempt to recover from other errors
+        except Exception:
+            self.logger.warning("Error with primary IP address server. An unknown error has occurred. \
+                                            Attempting to access fallback server in 30 seconds")
+
+        # If an IP address was not obtained from the primary server, attempt to obtain one from the fallback server
+        if not success:
+            try:
+                # Attempt to get IP from fallback server
+                response = requests.get(self._fallback_server_url)
+                if response.status_code != 200:
+                    raise _GetIPResponseError
+                else:
+                    success = True
+
+            # Except an incorrect response
+            except _GetIPResponseError:
+                self.logger.critical("Error with fallback IP address server. A response code other than 200 was \
+                                        received. Application will exit.")
+
+            # Log other errors
+            except Exception:
+                self.logger.critical("Error with fallback IP address server. An unknown error has occurred. \
+                                        Application will exit ")
+
+        # Check an ip address was received
+        if success:
+            ip_address = response.content.decode("utf-8").strip()
+        else:
+            # If not raise error
+            raise commonexception.FatalError("An unrecoverable error occurred while retrieving the servers IP address")
+
+        return ip_address
+
+
+## Error class for response errors
+class _GetIPResponseError(Exception):
+    pass
---
+++ bin/tests/test_ipaccess.py
@@ -0,0 +1,26 @@
+import unittest
+import requests
+import bin
+from bin.ipaccess import GetIP
+
+
+class TestIPAccess(unittest.TestCase):
+
+    # Test case to determine that the ip access service works correctly
+    def testipauto(self):
+        # Test ipify
+        service = GetIP("https://api.ipify.org", "https://api.ipify.org")
+        ipify_output = service.get()
+        self.assertEqual(requests.get("https://api.ipify.org").content.decode("utf-8").strip(), ipify_output,
+                         "Incorrect output with ipify")
+
+        # Test curl my ip
+        service = GetIP("https://curlmyip.net/", "https://curlmyip.net/")
+        curlip_output = service.get()
+        self.assertEqual(requests.get("https://curlmyip.net/").content.decode("utf-8").strip(), curlip_output,
+                         "Incorrect output with curl my ip")
+
+        # Test that IP outputs match
+        self.assertEqual(ipify_output, curlip_output, "IP addresses don't match" + ipify_output + ", " + curlip_output)
+        print("OUTPUT OF IP ACCESS : " + ipify_output)
+
