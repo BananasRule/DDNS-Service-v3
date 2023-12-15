b5d24a2154084c321a3c246cfd17166383fced9a
Jacob <54694270+BananasRule@users.noreply.github.com>
2023-12-15 23:25:20 +0800

    Added a log fatal function

--- bin/commonexception.py
+++ bin/commonexception.py
@@ -1,3 +1,11 @@
 ## @exception FatalError Shared error to be thrown when program cannot continue
 class FatalError(Exception):
     pass
+
+## Function to record logg a fatal error and then raise the Fatal Error exception
+# @param logger Logger to use to use to record the error
+# @param message Message to log
+# @throws FatalError This should not be caught except by main
+def logfatal(logger, message):
+    logger.critical(message)
+    raise FatalError
