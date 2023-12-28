2a6033e6ad416331c1a8f929087b37471f651a31
Jacob <54694270+BananasRule@users.noreply.github.com>
2023-12-28 21:23:54 +0800

    Updated name to be more inline with conventions

--- bin/commonexception.py
+++ bin/commonexception.py
@@ -2,10 +2,11 @@
 class FatalError(Exception):
     pass
 
+
 ## Function to record logg a fatal error and then raise the Fatal Error exception
-# @param logger Logger to use to use to record the error
+# @param logger Logger to use to record the error
 # @param message Message to log
 # @throws FatalError This should not be caught except by main
-def logfatal(logger, message):
+def log_fatal(logger, message):
     logger.critical(message)
     raise FatalError
